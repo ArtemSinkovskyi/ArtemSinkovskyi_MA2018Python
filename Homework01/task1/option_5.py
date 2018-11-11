c50298b06aa8b69f9d117df21267c630648cd4a9
Artem Sinkovskyi <artem.sinkovskiy@gmail.com>
2018-11-11 23:43:50 +0200

    H01-T1 PEP8 is correctly in options 5

--- Homework01/task1/option_5.py
+++ Homework01/task1/option_5.py
@@ -1,28 +1,24 @@
 import random
 
-dictionaryOfKeyElementsInGame = \
+elementsInGame = \
     ["rock", "Spock", "paper", "lizard", "scissors"]
 
 
 def winner(user_option):
     computer_option = random.randint(0, 4)
+    print("\nYour variant is: ", elementsInGame[user_option],
+          "\nComputer variant is: ", elementsInGame[computer_option], )
     if user_option == computer_option:
-        print("\nYour variant is: ", dictionaryOfKeyElementsInGame[user_option], \
-              "\nComputer variant is: ", dictionaryOfKeyElementsInGame[computer_option], \
-              "\nDead heat")
-    elif computer_option == 0 and (user_option == 3 or user_option == 4) \
-            or computer_option == 1 and (user_option == 4 or user_option == 0) \
-            or computer_option == 2 and (user_option == 0 or user_option == 1) \
-            or computer_option == 3 and (user_option == 1 or user_option == 2) \
-            or computer_option == 4 and (user_option == 2 or user_option == 3):
-        print("\nYour variant is: ", dictionaryOfKeyElementsInGame[user_option], \
-              "\nComputer variant is: ", dictionaryOfKeyElementsInGame[computer_option], \
-              "\nComputer is WINNER")
+        print("Dead heat")
+    elif computer_option == 0 and (user_option == (3 or 4)) \
+            or computer_option == 1 and (user_option == (4 or 0)) \
+            or computer_option == 2 and (user_option == (0 or 1)) \
+            or computer_option == 3 and (user_option == (1 or 2)) \
+            or computer_option == 4 and (user_option == (2 or 3)):
+        print("Computer is WINNER")
     else:
-        print("\nYour variant is: ", dictionaryOfKeyElementsInGame[user_option], \
-              "\nComputer variant is: ", dictionaryOfKeyElementsInGame[computer_option], \
-              "\nYou are WINNER")
+        print("You are WINNER")
 
 
-for i in dictionaryOfKeyElementsInGame:
-    winner(dictionaryOfKeyElementsInGame.index(i))
+for i in elementsInGame:
+    winner(elementsInGame.index(i))
