ed7e81ae208870c8f5802c6062a32767a7763629
Artem Sinkovskyi <artem.sinkovskiy@gmail.com>
2018-11-15 23:16:22 +0200

    ANT Add line chart (lib matplotlib)

---
+++ AnotherTasks/README.md
@@ -0,0 +1,6 @@
+### Additional materials
+
+## DIR: AnotherTasks
+
+### Visualization
+### plot_graph : https://www.youtube.com/watch?v=voLSnXi4hAI
---
+++ AnotherTasks/Visualization/plot_graph_matplotlib.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 6, 10, 12, 17]
+y = [0.2, 0.3, 0.4, 0.7, 0.9, 1.6, 0.5]
+plt.plot(x, y)
+plt.show()
