2ff16cebbf3d5995475e2cbb01f1d710268e24b2
Artem Sinkovskyi <artem.sinkovskiy@gmail.com>
2018-11-16 19:32:02 +0200

    ANT add Lingvo variables

---
+++ AnotherTasks/Visualization/linvo_var.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+y = [0, 1, 1, 0]
+x1 = [0, 0, 0.15, 0.25]
+x2 = [0.15, 0.25, 0.35, 0.45]
+x3 = [0.35, 0.45, 0.55, 0.65]
+x4 = [0.55, 0.65, 0.75, 0.85]
+x5 = [0.75, 0.85, 1, 1]
+plt.plot(x1, y)
+plt.plot(x2, y)
+plt.plot(x3, y)
+plt.plot(x4, y)
+plt.plot(x5, y)
+plt.show()
---
+++ AnotherTasks/Visualization/plot_graph_matplotlib.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(1, 5, 11)
+y = x ** 2
