4a2396dae57ea8044aec444aa08a810686f02f55
Artem Sinkovskyi <artem.sinkovskiy@gmail.com>
2018-12-04 15:54:44 +0200

    ANT work with matrix

--- AnotherTasks/university/gui.py
+++ AnotherTasks/university/gui.py
@@ -5,6 +5,8 @@ root.geometry('1320x680')
 canvas = Canvas(root, width=680, heigh=680, bg="grey")
 canvas.pack(side='right')
 
+
+
 label_number_x = Label(root, text='Введіть назву підприємства')
 label_number_x.place(x=0, y=10)
 text_number_x = Entry(root)
--- AnotherTasks/university/matrix_operations_numpy.py
+++ AnotherTasks/university/matrix_operations_numpy.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # np.ones - заполнение матрици размером 3 на 5 -> единицами
 matrix_subcategories = np.ones((3, 5))
@@ -42,3 +43,30 @@ example_array = np.array([[1, 4, 5], [1, 4, 5]], float)
 
 # Метод shape возвращает количество строк и столбцов в матрице (ГДЕ КОЛИЧЕСТВО ЭЛЛЕМЕНТОВ В СТРОКЕ -> ОДИНАКОВОЕ)
 count_row_and_coll = example_array.shape
+
+
+example_arr = [1, 4, 5, 6]
+
+
+y = [0, 1, 1, 0]
+x1 = [0, 0, 0.15, 0.25]
+plt.plot(x1, example_arr)
+
+s = y * x1[0]
+
+# x2 = [0.15, 0.25, 0.35, 0.45]
+# x3 = [0.35, 0.45, 0.55, 0.65]
+# x4 = [0.55, 0.65, 0.75, 0.85]
+# x5 = [0.75, 0.85, 1, 1]
+
+# multiplication_arr_el_to_x1 = arr * y[0]
+
+
+# plt.plot(x1, y)
+# plt.plot(x2, y)
+# plt.plot(x3, y)
+# plt.plot(x4, y)
+# plt.plot(x5, y)
+plt.show()
+
+# multiplication_arr_el_to_x1 = np.dot(arr, x1[0])
